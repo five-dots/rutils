91233b8fc4595b62eb6afe9bb1b5df5edc8468f5
shun <syun.asai@gmail.com>
2019-07-08 19:00:06 +0900

    Add new functions.

--- tests/testthat/test_time.R
+++ tests/testthat/test_time.R
@@ -15,3 +15,21 @@ test_that("make_duration() returns correct data.", {
   expect_error(make_duration(1))
   expect_error(make_duration("09:3000"))
 })
+
+test_that("make_dt_from_msec() returns correct data.", {
+
+  ## Prepare test data
+  msec <- c(1506378448618, 1506378448619)
+  test_data <- make_dt_from_msec(msec, tz = "America/New_York")
+
+  ## Type check
+  expect_is(test_data, "POSIXct")
+  ## Value check
+  expect_equal(test_data,
+               c(lubridate::ymd_hms("2017-09-25 18:27:28.618", tz = "America/New_York"),
+                 lubridate::ymd_hms("2017-09-25 18:27:28.619", tz = "America/New_York")))
+
+  ## Error check
+  expect_error(make_duration("hoge"))
+  expect_error(make_duration(1506378448618, 1))
+})
